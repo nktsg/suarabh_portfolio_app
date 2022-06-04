99df27fcf36f815fe03fee8aebd6ed7f91d1554f
Ankit Singh <ankitsingh26548@gmail.com>
2022-06-04 22:28:20 +0530

    Animation and SVG changes

---
+++ src/components/contact_item_arrow.js
@@ -0,0 +1,28 @@
+export default function ContactArrowIcon() {
+  return (
+    <>
+      <svg
+        width="12"
+        height="12"
+        viewBox="0 0 12 12"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M1 11L11 1"
+          stroke="#121212"
+          strokeWidth="2"
+          strokLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+        <path
+          d="M1 1H11V11"
+          stroke="#121212"
+          strokeWidth="2"
+          strokLinecap="round"
+          strokeLinejoin="round"
+        ></path>
+      </svg>
+    </>
+  );
+}
